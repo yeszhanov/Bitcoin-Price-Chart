01e15bc580947fc1ccb13259f942377925061e2b
Yerlan <yeszhanov@yahoo.com>
2019-05-22 05:23:09 +0600

    ADDED: getting bitcoin price

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import * as d3 from "d3"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null,
+  }
+
+  async getData() {
+    try {
+      const response = await fetch(
+        "https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01",
+      )
+      const json = await response.json()
+      this.parseData(json)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  parseData(data) {
+    var arr = []
+    for (let i in data.bpi) {
+      arr.push({
+        date: new Date(i),
+        value: data.bpi[i],
+      })
+    }
+    this.setState({
+      data: arr,
+    })
+  }
+
+  displayChart(data) {
+    console.log(data)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.data !== this.state.data) {
+      this.displayChart(this.state.data)
+    }
+  }
+  componentDidMount() {
+    this.getData()
+  }
+
+  render() {
+    return (
+      <div className="canvas">
+        <svg className="line-chart" />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
