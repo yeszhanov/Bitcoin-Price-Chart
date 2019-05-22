26dd73c098eb14e3a2eca12db4944e3362bc631f
Yerlan <yeszhanov@yahoo.com>
2019-05-22 08:12:17 +0600

    ADDED: d3 chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import * as d3 from "d3"
+
 import "./App.css"
 
 class App extends Component {
@@ -24,7 +25,7 @@ class App extends Component {
     for (let i in data.bpi) {
       arr.push({
         date: new Date(i),
-        value: data.bpi[i],
+        price: data.bpi[i],
       })
     }
     this.setState({
@@ -33,7 +34,43 @@ class App extends Component {
   }
 
   displayChart(data) {
-    console.log(data)
+    let height = 400
+    let width = 600
+
+    let maxDate = d3.max(data, d => d.date)
+
+    let minDate = d3.min(data, d => d.date)
+    let maxPrice = d3.max(data, data => data.price)
+
+    let y = d3
+      .scaleLinear()
+      .domain([0, maxPrice])
+      .range([height, 0])
+    let x = d3
+      .scaleTime()
+      .domain([minDate, maxDate])
+      .range([0, width])
+
+    let yAxe = d3.axisLeft(y)
+    let xAxe = d3.axisBottom(x)
+
+    let svg = d3
+      .select("svg")
+      .attr("height", "600")
+      .attr("width", "800")
+
+    let group = svg.append("g").attr("transform", "translate( 50 , 50)")
+    let line = d3
+      .line()
+      .x(d => x(d.date))
+      .y(d => y(d.price))
+
+    group.append("path").attr("d", line(data))
+    group
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxe)
+    group.append("g").call(yAxe)
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -48,6 +85,7 @@ class App extends Component {
   render() {
     return (
       <div className="canvas">
+        <h1> Bitcoin price chart</h1>
         <svg className="line-chart" />
       </div>
     )
